9d556e3e222ef65b97c8c10482c4a24930441ea8
Christina Larsen <clarsen1782@outlook.com>
2023-11-03 16:08:44 -0700

    Working on Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,78 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll()
+  .then((allCategories) => {
+    res.json(allCategories);
+  })
+  .catch((error) =>{
+    console.error(error);
+    res.status(500).json({message: 'Internal server error'});
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  const categoryId = parseInt(req.params.id); 
+  const category = categories.find((category) => category.id === categoryId); 
+
+  if (category) {
+    res.json(category);
+  } else {
+    res.status(404).json({ message: 'Category not found'});
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  const newCategory = req.body;
+  
+  if (!newCategory || !newCategory.name) {
+    res.status(400).json({ message: 'Category name is required'});
+  }
+  const newCategoryId = categories.length + 1;
+  const category = { id: newCategoryId, name: newCategory.name };
+  categories.push(category);
+  res.status(201).json(category);
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  const categoryId = parseInt(req.params.id);
+  const updatedCategory = req.body;
+  const category = categories.find((category) => category.id === categoryId);
+
+  if (!category) {
+    return res.status(404).json({ message: 'Category not found' });
+  }
+
+  if (!updatedCategory || !updatedCategory.name) {
+    return res.status(400).json({ message: 'Category name is required' });
+  }
+
+  category.name = updatedCategory.name;
+
+  res.json(category);
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  const categoryId = parseInt(req.params.id);
+
+  Category.destroy({
+    where: {
+      id: categoryId,
+    }
+  })
+    .then((rowsDeleted) => {
+      if (rowsDeleted === 0) {
+        res.status(404).json({ message: 'Category not found' });
+      } else {
+        res.status(204).end();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,35 +1,39 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll()
+  .then((allProducts) => {
+    res.json(allProducts);
+  })
+  .catch((error) =>{
+    console.error(error);
+    res.status(500).json({message: 'Internal server error'});
+  });
 });
 
-// get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  const productId = parseInt(req.params.id);
+  .then((product) => {
+    if (product) {
+      res.json(product);
+    }else {
+      res.status(404).json({message: 'Product not found'});
+    }
+  })
+  .catch((error) => {
+      console.error(error);
+      res.status(500).json({message:'Internal server error'});
+    });
 });
 
-// create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  const newProduct = req.body;
+
+  Product.create(newProduct)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (newProduct.tagIds && newProduct.tagIds.length) {
+        const productTagIdArr = newProduct.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
@@ -37,19 +41,15 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
+      res.status(201).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       res.status(400).json(err);
     });
 });
 
-// update product
 router.put('/:id', (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -61,7 +61,6 @@ router.put('/:id', (req, res) => {
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
-          // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
           .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -72,11 +71,9 @@ router.put('/:id', (req, res) => {
             };
           });
 
-            // figure out which ones to remove
           const productTagsToRemove = productTags
           .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
           .map(({ id }) => id);
-                  // run both actions
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
@@ -87,13 +84,29 @@ router.put('/:id', (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  const productId = parseInt(req.params.id);
+
+  Product.destroy({
+    where: {
+      id: productId,
+    }
+  })
+    .then((rowsDeleted) => {
+      if (rowsDeleted === 0) {
+        res.status(404).json({ message: 'Product not found' });
+      } else {
+        res.status(204).end();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
 
 module.exports = router;
